ab33397396a299987c48a21dbce1f29411523646
Kapoor-0905 <500097111@stu.upes.ac.in>
2023-01-05 23:45:18 +0530

    changes done

--- event_planning_app/lib/pages/home.dart
+++ event_planning_app/lib/pages/home.dart
@@ -1,4 +1,6 @@
-import 'package:event_planning_app/widgets/bottomNav.dart';
+import 'package:event_planning_app/pages/login.dart';
+import 'package:event_planning_app/pages/profile.dart';
+import 'package:event_planning_app/pages/register.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -9,63 +11,219 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  var currentIndex = 1;
-  var currentPage;
+  var currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(
+          "Welcome!",
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.w500,
+            color: Colors.black,
+          ),
+        ),
+        actions: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(right: 20.0),
+            child: GestureDetector(
+              onTap: () {},
+              child: Icon(
+                Icons.settings,
+                color: Color.fromRGBO(161, 117, 97, 2),
+              ),
+            ),
+          )
+        ],
+      ),
       body: Column(
         children: [
-          Container(
-            color: Theme.of(context).primaryColor,
-            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    //controller: searchController,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'courier new',
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                    ),
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: "Search Groups...",
-                      hintStyle: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontFamily: 'courier new',
-                        fontWeight: FontWeight.w400,
+          Center(
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.95,
+              height: MediaQuery.of(context).size.height * 0.07,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                border: Border.all(
+                  color: Colors.black.withOpacity(0.2),
+                ),
+              ),
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+              child: Row(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      //SearchMethod();
+                    },
+                    child: Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        color: Colors.black.withOpacity(0.2),
+                        borderRadius: BorderRadius.circular(40),
+                      ),
+                      child: Icon(
+                        Icons.search,
+                        color: Colors.black,
                       ),
                     ),
                   ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    //SearchMethod();
-                  },
-                  child: Container(
-                    width: 40,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      color: Colors.white.withOpacity(0.2),
-                      borderRadius: BorderRadius.circular(40),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    // decoration: BoxDecoration(
+                    //   borderRadius: BorderRadius.circular(20),
+
+                    // ),
+                    child: Expanded(
+                      child: TextField(
+                        //controller: searchController,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: "Lexend",
+                          fontWeight: FontWeight.w300,
+                          fontSize: 17,
+                        ),
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Search Groups...",
+                          hintStyle: TextStyle(
+                            color: Colors.black,
+                            fontFamily: "Lexend",
+                            fontSize: 18,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
                     ),
-                    child: Icon(
-                      Icons.search,
-                      color: Colors.white,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(161, 117, 97, 2).withOpacity(0.8),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.add),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+
+          // isLoading
+          //     ? Center(
+          //         child: CircularProgressIndicator(
+          //           color: Theme.of(context).primaryColor,
+          //         ),
+          //       )
+          //     : groupList(),
+        ],
+      ),
+      bottomNavigationBar:
+          // BottomNavigationBar(
+          //   items: const <BottomNavigationBarItem>[
+          //     BottomNavigationBarItem(
+          //       icon: Icon(Icons.home),
+          //       label: 'Home',
+          //     ),
+          //     BottomNavigationBarItem(
+          //       icon: Icon(Icons.business),
+          //       label: 'Business',
+          //     ),
+          //     BottomNavigationBarItem(
+          //       icon: Icon(Icons.school),
+          //       label: 'School',
+          //     ),
+          //   ],
+          //   currentIndex: _selectedIndex,
+          //   selectedItemColor: Colors.amber[800],
+          //   onTap: _onItemTapped,
+          // ),
+
+          Container(
+        margin: EdgeInsets.all(20),
+        height: size.width * .155,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(.15),
+              blurRadius: 30,
+              offset: Offset(0, 10),
+            ),
+          ],
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: ListView.builder(
+          itemCount: listOfIcons.length,
+          scrollDirection: Axis.horizontal,
+          padding: EdgeInsets.symmetric(horizontal: size.width * .07),
+          itemBuilder: (context, index) => InkWell(
+            onTap: () {
+              setState(
+                () {
+                  currentIndex = index;
+                },
+              );
+            },
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                AnimatedContainer(
+                  duration: Duration(milliseconds: 1500),
+                  curve: Curves.fastLinearToSlowEaseIn,
+                  margin: EdgeInsets.only(
+                    bottom: index == currentIndex ? 0 : size.width * .029,
+                    right: size.width * .06,
+                    left: size.width * .06,
+                  ),
+                  width: size.width * .128,
+                  height: index == currentIndex ? size.width * .014 : 0,
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(161, 117, 97, 1),
+                    borderRadius: BorderRadius.vertical(
+                      bottom: Radius.circular(10),
                     ),
                   ),
                 ),
+                IconButton(
+                  onPressed: () {
+                    setState(
+                      () {
+                        currentIndex = index;
+                      },
+                    );
+                  },
+                  icon: Icon(
+                    listOfIcons[index],
+                    color: index == currentIndex
+                        ? Color.fromRGBO(161, 117, 97, 1)
+                        : Colors.black.withOpacity(.3),
+                    size: size.width * .07,
+                  ),
+                ),
+                SizedBox(height: size.width * .03),
               ],
             ),
           ),
-        ],
+        ),
       ),
-      bottomNavigationBar: BottomNav(),
     );
   }
+
+  List<IconData> listOfIcons = [
+    Icons.home_rounded,
+    Icons.calendar_month_rounded,
+    Icons.person_rounded,
+  ];
 }
